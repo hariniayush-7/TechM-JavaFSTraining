6d07e22f643b90792b973257f7659a29fa0f44ab
Harini A <140332509+hariniayush-7@users.noreply.github.com>
2025-02-13 21:44:41 +0530

    Add files via upload

---
+++ Week 3(13th feb)/List/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ArrayListthread al=new ArrayListthread();
+        //al.start();
+
+        LinkedListThread ll=new LinkedListThread();
+        //ll.start();
+
+        VectorListThread vl=new VectorListThread();
+        //vl.start();
+
+        StackThread sl=new StackThread();
+        sl.start();
+
+    }
+    }
