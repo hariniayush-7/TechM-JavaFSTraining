72faaa5131f2777cec59549df6e3a71168d620a6
Harini A <140332509+hariniayush-7@users.noreply.github.com>
2025-02-20 21:48:21 +0530

    Add files via upload

---
+++ Week 4(20th feb)/Session_Management_Techniques/HTTPSession/Validate.java
@@ -0,0 +1,23 @@
+package mypack;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class Validate extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String name = request.getParameter("user");
+        String pass = request.getParameter("pass");
+        
+        if(pass.equals("1234"))
+        {
+            //creating a session
+            HttpSession session = request.getSession();
+            session.setAttribute("user", name);
+            response.sendRedirect("Welcome"); }    }}
---
+++ Week 4(20th feb)/Session_Management_Techniques/HTTPSession/Welcome.java
@@ -0,0 +1,22 @@
+package mypack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class Welcome extends HttpServlet {
+	 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
+        String user = (String)session.getAttribute("user");
+        out.println("Hello "+user);
+    }
+}
