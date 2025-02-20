01b7b1a57fe2cf15de56e99eccbe9b49d62e0ec4
Harini A <140332509+hariniayush-7@users.noreply.github.com>
2025-02-20 22:27:30 +0530

    Add files via upload

---
+++ Week 4(20th feb)/Session_Management_Techniques/Hidden_Form_Fields/First.java
@@ -0,0 +1,25 @@
+package hiddenField;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class First extends HttpServlet {
+
+	  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        response.setContentType("text/html;charset=UTF-8");
+	        PrintWriter out = response.getWriter();
+	        
+		//getting value submitted in form from HTML file
+	        String user = request.getParameter("user");
+	//creating a new hidden form field
+	        out.println("<form action='Second'>");
+	        out.println("<input type='hidden' name='user' value='"+user+"'>");
+	        out.println("<input type='submit' value='submit' >");
+	        out.println("</form>");    }}
