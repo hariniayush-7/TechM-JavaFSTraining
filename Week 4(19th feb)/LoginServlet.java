e5e2856d4ff51273a9d6bd1ced2d2256d1aa4e2c
Harini A <140332509+hariniayush-7@users.noreply.github.com>
2025-02-20 10:47:46 +0530

    Add files via upload

---
+++ Week 4(19th feb)/LoginServlet.java
@@ -0,0 +1,65 @@
+package mypack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ThirdExample
+ */
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.println("username: " + username);
+		System.out.println("password: " + password);
+		PrintWriter writer = response.getWriter();
+		String htmlRespone = "<html>";
+		        htmlRespone += "<h2>Your username is: " + username + "<br/>";      
+		        htmlRespone += "Your password is: " + password + "</h2></html>";    
+		          writer.println(htmlRespone);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.println("username: " + username);
+		System.out.println("password: " + password);
+		PrintWriter writer = response.getWriter();
+		String htmlRespone = "<html>";
+		        htmlRespone += "<h2>Your username is: " + username + "<br/>";      
+		        htmlRespone += "Your password is: " + password + "</h2></html>";    
+		          writer.println(htmlRespone);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+	}
