2416e694893725d314b24ff93675d9a2e91b0ed2
Harini A <140332509+hariniayush-7@users.noreply.github.com>
2025-02-21 14:39:20 +0530

    Add files via upload

---
+++ Week 4(21th feb)/JSP_Counter/Counter.java
@@ -0,0 +1,26 @@
+package jspPackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/jspCount")
+
+public class Counter extends HttpServlet {
+    int accesses = 0;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        accesses++;
+        out.print("Number of times this servlet has been accessed:" + accesses);
+    }
+}
