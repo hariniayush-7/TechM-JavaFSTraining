65d09e6e015d090fb2967767c5514bda23d52148
Harini A <140332509+hariniayush-7@users.noreply.github.com>
2025-02-27 14:29:01 +0530

    Create c1.jsx

---
+++ Week 5(27th feb)/TASK-2/c1.jsx
@@ -0,0 +1,10 @@
+import {useState} from 'react'
+import "./App.css";
+export default function Component1()
+{
+    return (
+        <>
+           <h1>This is the first component</h1>
+        </>
+      )
+}
