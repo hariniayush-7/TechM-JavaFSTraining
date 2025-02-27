02cc568229d22207888792ec94adf606db83d63b
Harini A <140332509+hariniayush-7@users.noreply.github.com>
2025-02-27 14:32:54 +0530

    Update main.jsx

--- Week 5(27th feb)/TASK-1/main.jsx
+++ Week 5(27th feb)/TASK-1/main.jsx
@@ -1,21 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import Task1 from './27th_feb/task1.jsx'
-// import Hi from './27th_feb/hi.jsx'
-// import Gm from './27th_feb/gm.jsx'
-// import Today from './27th_feb/today.jsx'
-// import Task1Completed from './27th_feb/task1Complete.jsx'
-// import ABComponent2 from './27thTasks/uuid.jsx'
-import Component1 from './c1.jsx'
-import Component2 from './f1/c2.jsx'
-
-
+import Task1 from './27th_feb/task1.jsx'
+import Hi from './27th_feb/hi.jsx'
+import Gm from './27th_feb/gm.jsx'
+import Today from './27th_feb/today.jsx'
+import Task1Completed from './27th_feb/task1Complete.jsx'
+import ABComponent2 from './27thTasks/uuid.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
- <Component1/>
- <Component2/>
+    <Hi/>
+    <Gm/>
+    <Today/>
+    <Task1/>
+    <Task1Completed/>
+    <ABComponent2/>
     
   </StrictMode>,
 )
