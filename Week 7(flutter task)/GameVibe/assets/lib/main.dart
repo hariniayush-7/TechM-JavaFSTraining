927f49230f64f033cbbc3ea223aab0b8b69855bd
Harini A <140332509+hariniayush-7@users.noreply.github.com>
2025-03-15 21:47:49 +0530

    Add files via upload

---
+++ Week 7(flutter task)/GameVibe/assets/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'localization.dart';
+import 'screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(GameVibeApp());
+}
+
+class GameVibeApp extends StatefulWidget {
+  const GameVibeApp({super.key});
+
+  @override
+  _GameVibeAppState createState() => _GameVibeAppState();
+}
+
+class _GameVibeAppState extends State<GameVibeApp> {
+  Locale _locale = Locale('en', '');
+
+  void _switchLanguage() {
+    setState(() {
+      if (_locale.languageCode == 'en') {
+        _locale = Locale('es', '');
+      } else {
+        _locale = Locale('en', '');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GameVibe',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(onLanguageSwitch: _switchLanguage),
+      locale: _locale,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', ''), // English
+        Locale('es', ''), // Spanish
+      ],
+    );
+  }
+}
